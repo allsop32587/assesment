0455f736e8d83c4b519e4111ec940fcfeb5b5441
user <email>
2025-06-02 01:07:01 +0530

    adjust height

--- src/components/AluminiumSystems.jsx
+++ src/components/AluminiumSystems.jsx
@@ -1,11 +1,9 @@
 const AluminiumSystems = () => {
   return (
-    <div className='relative w-full h-screen'>
-      <img src='/office.svg' alt='Descriptive alt text' className='w-full h-screen object-cover' />
-
-      <div className='absolute inset-0 h-screen bg-black opacity-70' />
-
-      <div className='absolute bottom-0 left-0 text-white p-4 uppercase'>
+    <div className='relative h-auto lg:h-screen'>
+      <img src='/office.svg' alt='Descriptive alt text' className='w-full h-full object-cover' />
+      <div className='absolute inset-0 bg-black opacity-70' />
+      <div className='absolute bottom-0 left-0 p-4 text-white uppercase'>
         <div className='text-[16px] font-normal opacity-70'>SERVICES / ALUMINIUM SYSTEMS</div>
         <div className='text-[50px] font-medium'>Aluminum Systems</div>
         <div className='text-[20px] font-medium opacity-70'>
